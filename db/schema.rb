84bf13b3f5c2a7ff1a427c376f04eddd324b9f68
Kaizilege Karoma <kkaroma@gmail.com>
2022-01-29 14:39:25 +0300

    Change contents from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_202120) do
+ActiveRecord::Schema.define(version: 2022_01_29_113636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "news", force: :cascade do |t|
     t.string "title"
-    t.string "contents"
+    t.text "contents"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
